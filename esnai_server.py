e434ad2030ee24a43d47dcbcd61c929de4999d34
idrsesn21-beep <idrsesn21@gmail.com>
2025-10-17 02:24:00 +0300

    Delete esnai_server.py

--- esnai_server.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(_name_)
-
-@app.route('/')
-def home():
-    return "✅ ESN AI Cloud Aktif - Bilal tarafından oluşturuldu!"
-
-if _name_ == '_main_':
-    app.run(host='0.0.0.0', port=10000)
